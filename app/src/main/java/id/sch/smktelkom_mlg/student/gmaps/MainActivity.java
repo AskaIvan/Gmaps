b0cb6c140471bdb187fd278518af39f3c2fd668e
AskaIvan <aska_gunawan_24rpl@student.smktelkom-mlg.sch.id>
2017-11-08 21:14:31 +0700

    GMAPS KU LESSON 4 VER 1

--- app/src/main/java/id/sch/smktelkom_mlg/student/gmaps/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/student/gmaps/MainActivity.java
@@ -1,81 +1,38 @@
 package id.sch.smktelkom_mlg.student.gmaps;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
 
 
-public class MainActivity extends ActionBarActivity implements OnMapReadyCallback {
-
-    static final CameraPosition SEATTLE = CameraPosition.builder()
-            .target(new LatLng(47.6204, -122.2491))
-            .zoom(10)
-            .bearing(0)
-            .tilt(45)
-            .build();
-    GoogleMap m_map;
-    boolean mapReady = false;
-    LatLng renton = new LatLng(47.489805, -122.120502);
-    LatLng kirkland = new LatLng(47.7301986, -122.1768858);
-    LatLng everett = new LatLng(47.978748, -122.202001);
-    LatLng lynnwood = new LatLng(47.819533, -122.32288);
-    LatLng montlake = new LatLng(47.7973733, -122.3281771);
-    LatLng kent = new LatLng(47.385938, -122.258212);
-    LatLng showare = new LatLng(47.38702, -122.23986);
+public class MainActivity extends FragmentActivity implements OnStreetViewPanoramaReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        StreetViewPanoramaFragment streetViewPanoramaFragment =
+                (StreetViewPanoramaFragment) getFragmentManager()
+                        .findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+        // Set the panorama location on startup, when no
+        // panoramas have been loaded.
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap map) {
-        map.moveCamera(CameraUpdateFactory.newCameraPosition(SEATTLE));
-        //map.addPolyline(new PolylineOptions().geodesic(true).add(renton).add(kirkland).add(everett).add(lynnwood).add(montlake).add(kent).add(showare).add(renton));
-        //map.addPolygon(new PolygonOptions().add(renton, kirkland, everett, lynnwood).fillColor(Color.GREEN));
-        map.addCircle(new CircleOptions()
-                .center(renton)
-                .radius(5000)
-                .strokeColor(Color.GREEN)
-                .fillColor(Color.argb(64, 0, 255, 0)));
+        panorama.setPosition(new LatLng(36.0579667, -112.1430996));
+        StreetViewPanoramaCamera camera = new StreetViewPanoramaCamera.Builder()
+                .bearing(180)
+                .build();
+        panorama.animateTo(camera, 10000);
 
     }
-
 
 }
